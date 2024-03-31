6346acfc6777a9486a744d9342b3a00ca4f9d98b
[Sumit Chuhan] <[valid-email]>
2024-03-31 07:26:39 +0545

    using django crsf which help to get always a new token to prevent from forgery

--- userauths/views.py
+++ userauths/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from userauths.forms import UserRegisterForm
 
 def register_view(request):
+    if request.method == 'POST':
+        print("User is registered sucessfully")
+        
+    else:
+        print("User cannot be registered")
+
     form = UserRegisterForm()
+
     context = {
         'form': form,
     }
