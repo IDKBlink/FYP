b68a6d91dac4999500ee6f76e73273edb56312c9
[Sumit Chuhan] <[valid-email]>
2024-04-06 16:51:54 +0545

    making the class product and product image for inserting image and giving people discount, rating and status of the product.

--- core/models.py
+++ core/models.py
@@ -3,13 +3,36 @@ from shortuuid.django_fields import ShortUUIDField
 from django.utils.html import mark_safe
 from userauths.models import User
 
+STATUS_CHOICE = (
+    ("processing", "Processing"),
+    ("shipped", "Shipped"),
+    ("delivered", "Delivered"),
+)
+
+STATUS = (
+    ("draft", "Draft"),
+    ("disabled", "Disabled"),
+    ("rejected", "Rejected"),
+    ("in_review", "In Review"),
+    ("published", "Published"),
+)
+
+RATING = (
+    (1,  "★☆☆☆☆"),
+    (2,  "★★☆☆☆"),
+    (3,  "★★★☆☆"),
+    (4,  "★★★★☆"),
+    (5,  "★★★★★"),
+)
+
 def user_directory_path(instance, filename):
     return 'user_{0}/{1}'.format(instance.user.id, filename)
 
+
 class Category(models.Model):
     cid = ShortUUIDField(unique=True, length=10, max_length=20, prefix="cat", alphabet="abcdefgh12345")
-    title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to="category")
+    title = models.CharField(max_length=100, default="Food")
+    image = models.ImageField(upload_to="category", default="category.jpg")
 
     class Meta:
         verbose_name_plural = "Categories"
@@ -17,14 +40,22 @@ class Category(models.Model):
     def category_image(self):
         return mark_safe('<img src="%s" width="50" height="50" />' % (self.image.url))
 
+    def product_count(self):
+        return Product.objects.filter(category=self).count()
+
     def __str__(self):
         return self.title
-    
+
+class Tags(models.Model):
+    pass
+
 class Vendor(models.Model):
     vid = ShortUUIDField(unique=True, length=10, max_length=20, prefix="ven", alphabet="abcdefgh12345")
-    title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to="user_directory_path")
-    description = models.TextField(null=True, blank=True )
+
+    title = models.CharField(max_length=100, default="Nestify")
+    image = models.ImageField(upload_to=user_directory_path, default="vendor.jpg")
+    cover_image = models.ImageField(upload_to=user_directory_path, default="vendor.jpg")
+    description = models.TextField(null=True, blank=True, default="An Amazing Vendor")
 
     address = models.CharField(max_length=100, default="123 Main Street.")
     contact = models.CharField(max_length=100, default="+123 (456) 789")
@@ -35,6 +66,7 @@ class Vendor(models.Model):
     warranty_period = models.CharField(max_length=100, default="100")
 
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
     class Meta:
         verbose_name_plural = "Vendors"
@@ -44,3 +76,60 @@ class Vendor(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Product(models.Model):
+    pid = ShortUUIDField(unique=True, length=10, max_length=20, alphabet="abcdefgh12345")
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name="category")
+    vendor = models.ForeignKey(
+        Vendor, on_delete=models.SET_NULL, null=True, related_name="product")
+
+    title = models.CharField(max_length=100, default="Fresh Pear")
+    image = models.ImageField(upload_to=user_directory_path, default="product.jpg")
+    description = models.TextField(null=True, blank=True, default="This is the product")
+
+    price = models.DecimalField(max_digits=99999999999999, decimal_places=2, default="1.99")
+    old_price = models.DecimalField(max_digits=99999999999999, decimal_places=2, default="2.99")
+    
+    specifications = models.TextField(null=True, blank=True)
+    tags = models.ForeignKey(Tags, on_delete=models.SET_NULL, null=True)
+
+    product_status = models.CharField(choices=STATUS, max_length=10, default="in_review")
+
+    status = models.BooleanField(default=True)
+    in_stock = models.BooleanField(default=True)
+    featured = models.BooleanField(default=False)
+    digital = models.BooleanField(default=False)
+
+    sku = ShortUUIDField(unique=True, length=4, max_length=10, prefix="sku", alphabet="1234567890")
+
+    date = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Products"
+
+    def product_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % (self.image.url))
+
+    def __str__(self):
+        return self.title
+
+    def get_precentage(self):
+        new_price = (self.price / self.old_price) * 100
+        return new_price
+
+class ProductImages(models.Model):
+    images = models.ImageField(
+        upload_to="product-images", default="product.jpg")
+    product = models.ForeignKey(
+        Product, related_name="p_images", on_delete=models.SET_NULL, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Product Images"
+
+
+
