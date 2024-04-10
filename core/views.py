2ce81600b0b0db2f60a79aeadeb71b589ab78597
[Sumit Chuhan] <[valid-email]>
2024-04-10 18:11:33 +0545

    We can see the total number of product in the vendors available and their details.

--- core/views.py
+++ core/views.py
@@ -49,8 +49,11 @@ def vendor_list_view(request):
 
 def vendor_detail_view(request, vid):
     vendor = Vendor.objects.get(vid=vid)
+    products = Product.objects.filter(vendor=vendor, product_status="published")
 
     context = {
         "vendor": vendor,
+        "products": products,
     }
     return render(request, "core/vendor-detail.html", context)
+
