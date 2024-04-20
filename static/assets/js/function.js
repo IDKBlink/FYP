f7db3bcadd67c32c965affd53395544fdee78475
[Sumit Chuhan] <[valid-email]>
2024-04-20 13:08:11 +0545

    In js file, adding new code to create add to cart functionality.

--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -138,3 +138,38 @@ $(document).ready(function () {
     });
 });
 
+// Add to cart functionality
+$("#add-to-cart-btn").on("click", function(){
+    let quantity = $("#product-quantity").val()
+    let product_title = $(".product-title").val()
+    let product_id = $(".product-id").val()
+    let product_price = $("#current-product-price").text()
+    let this_val = $(this)
+
+    console.log("Quantity:", quantity);
+    console.log("Title:", product_title);
+    console.log("Price:", product_price);
+    console.log("ID:", product_id);
+    console.log("Currrent Element:", this_val);
+
+    $.ajax({
+        url: '/add-to-cart',
+        data:{
+            'id': product_id,
+            'qty': quantity,
+            'title': product_title,
+            'price': product_price,
+        },
+        dataType: 'json',
+        beforeSend: function(){
+            console.log("Adding Product to Cart...");
+        },
+        success: function(res){
+            this_val.html("Item added to Cart")
+            console.log("Added Product to Cart");
+
+        }
+
+        
+    })
+})
