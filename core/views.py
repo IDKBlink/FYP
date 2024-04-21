beaf577ab48db499c1204d1ecdddcbdd8352b5cb
[Sumit Chuhan] <[valid-email]>
2024-04-21 07:54:20 +0545

    If the cart is empty it directly take user to home page and show message that the cart is empty.

--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
 from django.db.models import Count, Avg
 from core.models import CartOrderProducts, Product, Category, Vendor, CartOrder, ProductImages, ProductReview, wishlist_model, Address
 from taggit.models import Tag
 from core.forms import ProductReviewForm
 from django.template.loader import  render_to_string
+from django.contrib import messages
 
 def index(request):
     #products = Product.objects.all().order_by("-id")
@@ -199,4 +200,11 @@ def add_to_cart(request):
         return JsonResponse({"error": "Missing required parameters"})
     
 def cart_view(request):
-     return render(request, "core/cart.html")
+    cart_total_amount = 0
+    if 'cart_data_obj' in request.session:
+        for p_id, item in request.session['cart_data_obj'].items():
+            cart_total_amount += int(item['qty']) * float(item['price'])
+        return render(request, "core/cart.html", {"cart_data":request.session['cart_data_obj'], 'totalcartitems': len(request.session['cart_data_obj']), 'cart_total_amount':cart_total_amount})
+    else:
+        messages.warning(request, "Your cart is empty")
+        return redirect("core:index")
