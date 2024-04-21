275b1d01016b6562ce76bbc4db5d50eaeec45ca2
[Sumit Chuhan] <[valid-email]>
2024-04-21 12:11:31 +0545

    Checkout page and to process the order in the page.

--- core/views.py
+++ core/views.py
@@ -244,4 +244,10 @@ def update_cart(request):
     return JsonResponse({"data": context, 'totalcartitems': len(request.session['cart_data_obj'])})
 
 def checkout_view(request):
-    return render(request, "core/checkout.html")
+    cart_total_amount = 0
+    # Checking if cart_data_obj session exists
+    if 'cart_data_obj' in request.session:
+        for p_id, item in request.session['cart_data_obj'].items():
+            cart_total_amount += int(item['qty']) * float(item['price'])
+        return render(request, "core/checkout.html")
+    return render(request, "core/checkout.html", {"cart_data":request.session['cart_data_obj'], 'totalcartitems': len(request.session['cart_data_obj']), 'cart_total_amount':cart_total_amount})
