492c700636abf6b12a314005057edadbd313a48b
[Sumit Chuhan] <[valid-email]>
2024-04-21 16:44:47 +0545

    Making a Contact us in the admin panel and adding fields like fullname, email, number, subject and message.

--- userauths/models.py
+++ userauths/models.py
@@ -11,3 +11,13 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+class ContactUs(models.Model):
+    full_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200) 
+    subject = models.CharField(max_length=200) 
+    message = models.TextField()
+
+    def __str__(self):
+        return self.full_name
